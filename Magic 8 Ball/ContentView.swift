53f8a2a4e2fcf34249daf8210e1b0891881b9029
Andrew <acastillonevarez@bsd220.org>
2022-12-15 19:02:50 -0600

    Initial Commit

---
+++ Magic 8 Ball/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Magic 8 Ball
+//
+//  Created by Andrew Castillo Nevarez on 12/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Magic 8 Ball/Magic_8_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_BallApp.swift
+//  Magic 8 Ball
+//
+//  Created by Andrew Castillo Nevarez on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Magic_8_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
